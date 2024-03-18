3b4bb93b0f3027ff5daf5bb768c686f7669a2194
root <allaa.amer.37@gmail.com>
2024-03-18 22:35:13 +0000

    change cd files

--- setup/init.sh
+++ setup/init.sh
@@ -3,7 +3,7 @@ set -e -o pipefail
 
 echo "Fetching IAM github-action-user ARN"
 userarn=$(aws iam get-user --user-name github-action-user | jq -r .User.Arn)
-
+cd
 # Download tool for manipulating aws-auth
 echo "Downloading tool..."
 curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
